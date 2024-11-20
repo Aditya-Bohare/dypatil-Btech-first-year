4287ad7989aaf7db52b86a1f2569f994d1cdc536
Aditya-Bohare <adityabohare1@gmail.com>
2024-11-21 00:54:53 +0530

    completed the conditonal statement

--- basic_c/conditional_statement.c
+++ basic_c/conditional_statement.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
 
 int main(){
-    printf("grade calculater");
+    int number;
+    printf("grade calculater\n");
+
+    printf("enter your marks:");
+    scanf("%d",&number);
+
+    if (number >= 95 && number <=100)
+        printf("A+");
+    else if(number >=90 && number < 95)
+        printf("A");
+    else if(number >=85 && number < 90)
+        printf("B+");
+    else if(number >=80 && number < 85)
+        printf("B");
+    else if(number >=70 && number < 80)
+        printf("C");
+    else if(number >=60 && number < 70)
+        printf("D");
+    else if(number >=40 && number < 60)
+        printf("E");
+    else if(number >= 0 && number < 40)
+        printf("F");
+    else if(number < 0)
+        printf("numbers cant be negative.");
+    else
+        printf("your marks is invalid plz correct it.");
     return 0;
 }
